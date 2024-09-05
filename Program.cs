12522b89793598fddebfcc66a84594c8b0a36f88
larsg <lars.gunnar@jobloop.no>
2024-09-05 12:39:22 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+class Movie
+{
+    public string Title { get; set; }
+
+    public Movie(string title)
+    {
+        Title = title;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        // Register the Movie list with the Dependency Injection Container
+        builder.Services.AddSingleton<List<Movie>>();
+        var app = builder.Build();
+
+        // READ: Get all movies
+        app.MapGet("/movies", (List<Movie> movies) => movies);
+        // CREATE: Adds a new movie
+        app.MapPost("/movies", (Movie? movie, List<Movie> movies) =>
+        {
+            if (movie == null)
+            {
+                return Results.BadRequest();
+            }
+
+            movies.Add(movie);
+
+            return Results.Created();
+        });
+
+        // DELETE: Delete a movie with id
+        app.MapDelete("/movies/{Id}", (int Id) => $"Delete movie with id: {Id}");
+        // UPDATE: Update a movie with id
+        app.MapPut("/movies/{Id}", (int Id) => $"Update movie with id: {Id}");
+
+        // System status
+        app.MapGet("/health", () => "System healthy");
+
+        app.Run();
+    }
+}
